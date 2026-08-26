b7db650a8402efba745887bef213502fd42a8089
LucasJSM <lucasjsm2001@gmail.com>
2026-08-25 22:05:37 -0300

    Refactor RGB channel splitting into a function for improved modularity

--- split_rgb_channels.py
+++ split_rgb_channels.py
@@ -1,11 +1,30 @@
 import cv2 as cv
+import sys
 
-img = cv.imread('./images/spiderman.jpg')
+'''
+Script to split the RGB channels of an image and save them as separate images.
+'''
 
-b, g, r = cv.split(img)
+def split_rgb_channels(img_path):
+	img = cv.imread(img_path)
 
-cv.imwrite('./images/spiderman_blue.jpg', b)
-cv.imwrite('./images/spiderman_red.jpg', r)
-cv.imwrite('./images/spiderman_green.jpg', g)
+	if img is None:
+		print(f"Erro: não foi possível carregar a imagem '{img_path}'")
+		sys.exit()
 
-print('Canais RGB separados e salvos com sucesso!')
+	b, g, r = cv.split(img)
+
+	cv.imwrite('./images/spiderman_blue.jpg', b)
+	cv.imwrite('./images/spiderman_red.jpg', r)
+	cv.imwrite('./images/spiderman_green.jpg', g)
+
+	print('Canais RGB separados e salvos com sucesso!')
+
+
+def main():
+	img_path = './images/spiderman.jpg'
+	split_rgb_channels(img_path)
+
+
+if __name__ == '__main__':
+	main()
