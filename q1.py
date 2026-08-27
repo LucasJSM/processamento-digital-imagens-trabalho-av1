c76cdd1867bd912f6c6a6bb6d4cc040b93d1a2e6
LucasJSM <lucasjsm2001@gmail.com>
2026-08-27 19:28:07 -0300

    Refactor show_image function

--- q1.py
+++ q1.py
@@ -1,28 +1,17 @@
 import cv2 as cv
-import sys
 
 def show_image(img_path):
     img = cv.imread(img_path)
 
-    if img is None:
-        print(f"Erro: não foi possível carregar a imagem '{img_path}'")
-        sys.exit();
-
-    tamanho = (404, 599)
-
-    img = cv.resize(img, tamanho)
-
     cv.namedWindow("Questao 1")
-    cv.moveWindow("Questao 1", 100, 50)
     cv.imshow("Questao 1", img)
     
     cv.waitKey()
 
     cv.destroyAllWindows()
 
-
 def main():
-    img_path = "./images/spiderman.jpg"
+    img_path = "./images/Lena.jpg"
     show_image(img_path)
 
 if __name__ == "__main__":
