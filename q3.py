4706c53d17268e7fe6ecbadc8b701112ee2aaba4
LucasJSM <lucasjsm2001@gmail.com>
2026-08-30 21:49:22 -0300

    Fix color mapping and grayscale rendering in show_image

--- q3.py
+++ q3.py
@@ -10,23 +10,20 @@ def show_image():
     img = cv.merge([blue, green, red])
 
     # Convertendo para RGB
-    blue = cv.cvtColor(blue, cv.COLOR_BGR2RGB)
-    green = cv.cvtColor(green, cv.COLOR_BGR2RGB)
-    red = cv.cvtColor(red, cv.COLOR_BGR2RGB)
     img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
     
     plt.subplot(2, 2, 1)
-    plt.imshow(red)
+    plt.imshow(red, cmap='gray')
     plt.title("Canal Vermelho")
     plt.axis("off")
 
     plt.subplot(2, 2, 2)
-    plt.imshow(green)
+    plt.imshow(green, cmap='gray')
     plt.title("Canal Verde")
     plt.axis("off")
 
     plt.subplot(2, 2, 3)
-    plt.imshow(blue)
+    plt.imshow(blue, cmap='gray')
     plt.title("Canal Azul")
     plt.axis("off")
 
@@ -35,6 +32,7 @@ def show_image():
     plt.title("Imagem Colorida")
     plt.axis("off")
 
+    plt.tight_layout()
     plt.show()
 
 def main():
